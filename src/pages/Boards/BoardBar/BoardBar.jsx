33f7ddf64815969e4452f24639336b70eda464fb
tlhoanganh1003 <tlhoanganh1003@gmail.com>
2024-07-09 18:23:22 +0700

    lesson_16

--- src/pages/Boards/BoardBar/BoardBar.jsx
+++ src/pages/Boards/BoardBar/BoardBar.jsx
@@ -6,11 +6,9 @@ import AddToDriveIcon from '@mui/icons-material/AddToDrive'
 import BoltIcon from '@mui/icons-material/Bolt'
 import FilterListIcon from '@mui/icons-material/FilterList'
 import { Tooltip } from '@mui/material'
-import Button from '@mui/material/Button'
-import PersonAddIcon from '@mui/icons-material/PersonAdd'
 import { capitalizeFirstLetter } from '~/utils/formatters'
 import BoardUserGroup from './BoardUserGroup'
-
+import InviteBoardUser from './InviteBoardUser'
 const MENU_STYLES = {
   color: 'white',
   bgcolor: 'transparent',
@@ -74,18 +72,9 @@ function BoardBar({ board }) {
       </Box>
 
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-        <Button
-          variant="outlined"
-          startIcon={<PersonAddIcon />}
-          sx={{
-            color: 'white',
-            borderColor: 'white',
-            '&:hover': { borderColor: 'white' }
-          }}
-        >
-          Invite
-        </Button>
-
+        {/* xử lý mời user vào làm thành viên của board */}
+        <InviteBoardUser boardId={board._id} />
+        {/* xử lý xem user là thành viên của board */}
         <BoardUserGroup boardUsers={board.FE_allUsers} />
       </Box>
     </Box>
